1133c65d6a13cbff6b9ac0b531eee8ed1fb54fdf
Jake Samuels <jakesamuels@Jakes-MacBook-Pro.local>
2023-02-20 13:22:08 +0000

    Create express get request to /api/categories

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require("express");
-
 const app = express();
+
+const categoriesController = require("./controllers/categoriesController");
+
+app.get("/api/categories", categoriesController.getCategories);
 
 module.exports = app;
