2a39a9fcd67dff493cb5cd684c431d9d64283faa
Jake Samuels <jakesamuels@Jakes-MacBook-Pro.local>
2023-02-23 09:49:08 +0000

    Changed status of 'Review not found' from 400 to 404

--- controllers/errorHandlingController.js
+++ controllers/errorHandlingController.js
@@ -8,7 +8,7 @@ exports.handlePsqlErrors = (err, req, res, next) => {
   if (err.code === "22P02") {
     res.status(400).send({ message: "Invalid review ID" });
   } else if (err.code === "23503") {
-    res.status(400).send({ message: "Review not found" });
+    res.status(404).send({ message: "Review not found" });
   } else next(err);
 };
 
