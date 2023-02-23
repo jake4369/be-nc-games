e04744df36a745245d56777c4544fa07861b894a
Jake Samuels <jakesamuels@Jakes-MacBook-Pro.local>
2023-02-23 11:21:48 +0000

    Handled error for incorrect data type in patch body

--- controllers/reviewsController.js
+++ controllers/reviewsController.js
@@ -56,6 +56,13 @@ exports.updateReview = (req, res, next) => {
   const { reviewId } = req.params;
   const { inc_votes } = req.body;
 
+  if (inc_votes && typeof inc_votes !== "number") {
+    return next({
+      status: 400,
+      message: "Invalid data type for inc_votes",
+    });
+  }
+
   reviewsModel
     .updateReview(reviewId, inc_votes)
     .then((review) => {
