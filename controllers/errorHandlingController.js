6f60bda1b0bb5b5eebd3afd7e6a7006fe3eae03d
Jake Samuels <jakesamuels@Jakes-MacBook-Pro.local>
2023-02-23 11:21:14 +0000

    Modified response message for incorrect key

--- controllers/errorHandlingController.js
+++ controllers/errorHandlingController.js
@@ -8,7 +8,7 @@ exports.handlePsqlErrors = (err, req, res, next) => {
   if (err.code === "22P02") {
     res.status(400).send({ message: "Invalid review ID" });
   } else if (err.code === "23502") {
-    res.status(400).send({ message: "Invalid patch body" });
+    res.status(400).send({ message: "Invalid key in patch body" });
   } else if (err.code === "23503") {
     res.status(404).send({ message: "Review not found" });
   } else next(err);
