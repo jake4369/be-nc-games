048d93edcbaa5839132ef9a19cab9b7e55e375d5
Jake Samuels <jakesamuels@Jakes-MacBook-Pro.local>
2023-02-25 20:09:44 +0000

    Add getUser model

--- models/usersModel.js
+++ models/usersModel.js
@@ -12,3 +12,24 @@ exports.getUsers = () => {
       return users;
     });
 };
+
+exports.getUser = (username) => {
+  return db
+    .query(
+      `
+      SELECT * FROM users
+      WHERE username = $1;
+    `,
+      [username]
+    )
+    .then((result) => {
+      const user = result.rows[0];
+      if (!user) {
+        return Promise.reject({
+          status: 404,
+          message: "User not found",
+        });
+      }
+      return user;
+    });
+};
