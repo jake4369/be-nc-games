7587ef03aeb43e1b2b5a569734da72a92799fa63
Jake Samuels <jakesamuels@Jakes-MacBook-Pro.local>
2023-02-25 20:41:08 +0000

    Add updateComment model

--- models/commentsModel.js
+++ models/commentsModel.js
@@ -21,3 +21,26 @@ exports.deleteComment = (commentId) => {
       return comment;
     });
 };
+
+exports.updateComment = (incVotes, commentId) => {
+  return db
+    .query(
+      `
+      UPDATE comments
+      SET votes = votes + $1
+      WHERE comment_id = $2
+      RETURNING *;
+    `,
+      [incVotes, commentId]
+    )
+    .then((result) => {
+      if (result.rowCount === 0) {
+        return Promise.reject({
+          status: 404,
+          message: `Comment not found`,
+        });
+      }
+      const review = result.rows[0];
+      return review;
+    });
+};
