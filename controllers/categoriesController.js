ced3e3ab67811be6fcf48fc51fae8304f1ae568b
Jake Samuels <samuels242@googlemail.com>
2023-03-12 13:20:39 +0000

    Add addCategory controller for POST /api/categories

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -11,3 +11,22 @@ exports.getCategories = (req, res, next) => {
     })
     .catch((error) => next(error));
 };
+
+exports.addCategory = (req, res, next) => {
+  const { slug, description } = req.body;
+
+  if (!slug || !description) {
+    return res.status(400).json({
+      message: "Both 'slug' and 'description' keys are required.",
+    });
+  }
+
+  categoriesModel
+    .addCategory(slug, description)
+    .then((category) => {
+      res.status(201).json({
+        category: category,
+      });
+    })
+    .catch((error) => next(error));
+};
